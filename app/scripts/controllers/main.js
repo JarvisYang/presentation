6dfeabe2cec8a35225092c353ba982cf42949ea3
JarvisYang <ywj200844@gmail.com>
2014-09-13 18:38:27 +0800

    v 0.02 第二屏

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,13 +9,14 @@
  */
 var presentationApp = angular.module('presentationApp');
 
-  presentationApp.controller('MainCtrl', function ($scope) {
+  presentationApp.controller('MainCtrl', function ($scope,$timeout,$interval) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
+    	$scope.screenClickCount2 = 0;
     	$scope.screenEnter = ['',''];
 	$scope.transfer = '';
 
@@ -23,11 +24,71 @@ var presentationApp = angular.module('presentationApp');
 		$scope.screenEnter[0] = 'screenEnter0';
 		$scope.screenEnter[1] = 'screenEnter1';
 		//$scope.transfer = 'transferEnter';
+		$timeout(function(){
+			$scope.screenHide1 = {'display':'none'};
+		},1000);
+		// $scope.screenStyle2 = {'display':'inline-block'};
 		$scope.screenEnter2 = 'screenEnter2';
-		$scope.webTextBoxDisplay = {'display':'inline-block'};
 		$scope.webTextBoxEnter = 'webTextBoxEnter';
 		$scope.textWEnter = 'textWEnter';
 		$scope.textEEnter = 'textEEnter';
 		$scope.textBEnter = 'textBEnter';
+	};
+
+	$scope.screenClick2 = function(){
+		switch($scope.screenClickCount2){
+			case 0:
+				$scope.textEEnter = 'textELeave';
+				$scope.textBEnter = 'textBLeave';
+				$scope.textHEnter = 'textHEnter';
+				$scope.textYEnter = 'textYEnter';
+				$scope.textQEnter = 'textQEnter';
+				$scope.screenClickCount2++;
+				break;
+			case 1 :
+				$scope.webTextBoxEnter = 'webTextBoxMove';
+				$scope.changeTime();
+				$scope.screenClickCount2++;
+				break;
+			case 2:
+				$scope.ieEnter = 'ieEnter';
+				$scope.screenClickCount2++;
+				break;
+
+		}
+
+	$scope.changeTime = function(){
+		var countTime = 120;
+		var num = [1,2,3];
+		$timeout(function() {
+			var setTime = $interval(function(){
+				if(countTime > 20){
+					num[0] = num[0] === 1?9:num[0]-1;
+				}
+				else{
+					num[0] = 4;
+				}
+				if(countTime > 10){
+					num[1] = num[1] ===9?1:num[1]+1;
+				}
+				else{
+					num[1] = 0;
+				}
+				if(countTime > 0){
+					num[2] = num[2]?num[2]-1:9;
+				}
+				else{
+					num[2] = 0;
+				}
+				if(countTime < 0){
+					$interval.cancel(setTime);
+				}
+				else{
+					$scope.screenChangeTime2 = String(num[0])+String(num[1])+String(num[2])+'kb/s';
+				}
+				countTime--;
+			},5);
+		}, 800);
+	};
 	};
   });
